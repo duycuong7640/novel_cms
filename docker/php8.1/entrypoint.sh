10ba99b0240578e214ae617e3b2874d4ba1c2f0a
Nguyễn Duy Cường <cuongnd@flextech.vn>
2024-06-03 09:19:11 +0700

    First commit

--- docker/php8.1/entrypoint.sh
+++ docker/php8.1/entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Wait for MySQL to be ready
-while ! mysqladmin ping -h"$DB_HOST" --silent; do
-    sleep 1
+while ! mysqladmin ping -h"dicnovel_db" --silent; do
+    echo "Waiting for database connection..."
+    sleep 2
 done
 
-# Install composer dependencies
-composer install
-
-# Run Laravel migrations
+# Run migrations
 php artisan migrate --force
 
 # Start Apache
-exec "$@"
+apache2-foreground
